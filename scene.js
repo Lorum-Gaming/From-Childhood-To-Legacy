560e6477667eae3423bfe350e2df7474b13b624a
Eduardo Ferraz <edupferraz@gmail.com>
2023-03-13 01:59:15 +0000

    Add animations and add temp assets

--- scene.js
+++ scene.js
@@ -3,100 +3,79 @@ export default class OpeningScene extends Phaser.Scene {
         super("opening");
         this.platforms;
         this.cursors;
-        this.stars;
         this.player;
-        this.bombs;
-        this.score = 0;
-        this.scoreText;
         this.gameOver = false;
     }
 
     preload () {
-        this.load.image('sky', 'assets/sky.png');
+        this.load.image('background', 'assets/packageSprites/Background_0.png');
         this.load.image('ground', 'assets/platform.png');
-        this.load.image('star', 'assets/star.png');
-        this.load.image('bomb', 'assets/bomb.png');
         this.load.spritesheet('dude', 'assets/dude.png',
-        { frameWidth: 32, frameHeight: 48});
+        { frameWidth: 32, frameHeight: 32});
+
+        this.load.spritesheet('dudeLeft', 'assets/dudeLeft.png',
+        { frameWidth: 32, frameHeight: 32});
     }
 
     create() {
-        this.add.image(400, 300, 'sky');
+        this.add.image(500, 300, 'background');
         this.platforms = this.physics.add.staticGroup();
         this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-    
     
         this.platforms.create(600, 400, 'ground');
         this.platforms.create(50, 250, 'ground');
         this.platforms.create(750, 220, 'ground');
     
-    
-        this.player = this.physics.add.sprite(100, 450, 'dude'); // (x, y, gameObject)
-        this.player.setBounce(0.2);
+        this.player = this.physics.add.sprite(100, 450, 'dude').setScale(2); // (x, y, gameObject)
+        this.player.setBounce(0);
         this.player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'idle',
+            frames: this.anims.generateFrameNumbers('dude', {
+                start: 0, end: 1, 
+            }),
+            frameRate: 5,
+            repeat: -1
+        })
 
         this.anims.create({
             key: 'left',
             frames: this.anims.generateFrameNumbers('dude', {
-                start: 0, end: 3
+                start: 25, end: 31,
             }),
             frameRate: 10,
             repeat: -1  // loop
         })
-    
-        this.anims.create({
-            key: 'turn',
-            frames: [{key: 'dude', frame: 4}],
-            frameRate: 20
-        })
-    
+
         this.anims.create({
             key: 'right',
             frames: this.anims.generateFrameNumbers('dude', {
-                start: 5, end: 8
+                start: 25, end: 31
             }),
             frameRate: 10,
             repeat: -1
         })
 
+        this.anims.create({
+            key: 'jump',
+            frames: this.anims.generateFrameNumbers('dude', {
+                start: 40, end: 43
+            }),
+            frameRate: 10,
+        })
+
+        this.anims.create({
+            key: 'fall',
+            frames: this.anims.generateFrameNumbers('dude', {
+                start: 45, end: 45
+            }),
+            frameRate: 10,
+        })
         
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        
-        this.stars = this.physics.add.group({
-            key: 'star',
-            repeat: 11,
-            setXY: { x: 12, y: 0, stepX: 70 }
-        });
-
-        this.stars.children.iterate(function (child) {
-            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-        });
-
-        this.bombs = this.physics.add.group();
-
-        this.scoreText = this.add.text(16, 16, 'Score: 0', {fontSize: '32px', fill: '#000'});
-
         this.physics.add.collider(this.player, this.platforms);
-        this.physics.add.collider(this.stars, this.platforms);
-        this.physics.add.collider(this.bombs, this.platforms);
-
-        // What is overlap?
-        this.physics.add.overlap(
-            this.player,
-            this.stars,
-            this.collectStar,
-            null,
-            this
-        );
-
-        this.physics.add.collider(
-            this.player,
-            this.bombs,
-            this.hitBomb,
-            null,
-            this
-        );
     }
 
     update() {
@@ -118,45 +97,17 @@ export default class OpeningScene extends Phaser.Scene {
         if (this.cursors.left.isDown) {
             this.player.setVelocityX(-160);
             this.player.anims.play('left', true);
+            
         } else if (this.cursors.right.isDown) {
             this.player.setVelocityX(160);     
             this.player.anims.play('right', true);
         } else {
             this.player.setVelocityX(0);     
-            this.player.anims.play('turn');
-        }
+            this.player.anims.play('idle', true);
+        } 
 
         if (this.cursors.up.isDown && this.player.body.touching.down) {
             this.player.setVelocityY(-450);
-        }   
-    }
-
-    collectStar(player, star) {
-        star.disableBody(true, true);
-        this.score += 10;
-        this.scoreText.setText("Score: " + this.score);
-
-        if(this.stars.countActive(true) === 0) {
-            this.stars.children.iterate(function(child) {
-                child.enableBody(true, child.x, 0, true, true);
-            });
-
-            var x = 
-                player.x < 400 
-                    ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-            
-            var bomb = this.bombs.create(x, 16, "bomb");
-            bomb.setBounce(1);
-            bomb.setCollideWorldBounds(true);
-            bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-            bomb.allowGravity = false;
         }
-    }
-
-    hitBomb(player, bomb) {
-        this.physics.pause();
-        player.setTint(0xff0000);
-        player.anims.play('turn');
-        this.gameOver = true;
     }
 }
